973f1768311f857d442fbc7cafc33a2d58dd3456
Владислав Рубан <ruban@rnd.stcnet.ru>
2026-04-12 01:05:08 +0300

    feat(selenium-2): impovemnts to PageOgject pattern

---
+++ src/shop/category_page.py
@@ -0,0 +1,44 @@
+"""Product category page."""
+from typing import ClassVar
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import Select
+
+from src.shop.base_page import BasePage
+
+
+class CategoryPage(BasePage):
+    """Product category page."""
+
+    OPTIONS: ClassVar[set[str]] = {
+        "Default",
+        "Name (A - Z)",
+        "Name (Z - A)",
+        "Price (Low > High)",
+        "Price (High > Low)",
+        "Rating (Highest)",
+        "Rating (Lowest)",
+        "Model (A - Z)",
+        "Model (Z - A)",
+    }
+
+    def __init__(
+        self,
+        browser: WebDriver,
+        base_url: str,
+        path: str,
+        title: str,
+    ):
+        super().__init__(browser, base_url, path)
+        self.title = title
+
+    def is_loaded(self) -> bool:
+        """Method that verify the category is shown."""
+        self.get_element((By.XPATH, f"//h2[text()='{self.title}']"))
+        self.get_element((By.CLASS_NAME, "product-thumb"))
+        self.get_element((By.ID, "input-sort"))
+        sort_dropdown = self.get_element((By.ID, "input-sort"))
+        select = Select(sort_dropdown)
+        actual_options = {option.text for option in select.options}
+        return actual_options == self.OPTIONS
